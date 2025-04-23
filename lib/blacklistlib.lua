fd99c48196ada20413c7abb8a4c643a7860d6516
TheRealNova422-real <polaris422@outlook.com>
2025-04-23 15:03:52 +1000

    i don't think these are necessary, actually (will have to see)

--- lib/blacklistlib.lua
+++ lib/blacklistlib.lua
@@ -2,15 +2,7 @@ function Cryptid.blibblock(list, target, remove)
 	return nil
 end
 
-function Cryptid.blibadd(list, target, key, remove)
-	return nil
-end
-
 function Cryptid.blibblocktype(list, target, key, remove)
-	return nil
-end
-
-function Cryptid.blibgetalias(list, target)
 	return nil
 end
 
