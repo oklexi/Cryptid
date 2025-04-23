10f44e92768ef62e06ad2f844c848d547b01cc68
TheRealNova422-real <polaris422@outlook.com>
2025-04-23 14:59:26 +1000

    starting work on it (still not functional)

---
+++ lib/blacklistlib.lua
@@ -0,0 +1,19 @@
+function Cryptid.blibblock(list, target, remove)
+  return nil
+end
+
+function Cryptid.blibadd(list, target, key, remove)
+  return nil
+end
+
+function Cryptid.blibblocktype(list, target, key, remove)
+  return nil
+end
+
+function Cryptid.blibgetalias(list, target)
+  return nil
+end
+
+function Cryptid.blibgetblocked(list, target)
+  return nil
+end
